f977c03b1f670c0be72a90677463d856358f1df3
Malith Thilakarathne <malitht@embla.lk>
2016-09-21 11:17:01 +0530

    Populate turn js book with angular

--- AngularWrapper/app.js
+++ AngularWrapper/app.js
@@ -2,8 +2,20 @@
  * Created by malitht on 9/20/2016.
  */
 
+
+var app = angular.module('myApp', []);
+
 $("#flipbook").turn({
     width: 400,
     height: 300,
     autoCenter: true
+});
+
+
+app.controller('myCtrl', function($scope) {
+    $scope.page1Content = "";
+    $scope.page2Content = "";
+    $scope.page3Content = "";
+
+
 });
